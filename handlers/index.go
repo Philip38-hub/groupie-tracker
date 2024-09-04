4f317f1978b02a5dceb0562660ffe704cc724202
Philip <oumaphilip01@gmil.com>
2024-09-04 10:22:51 +0300

    feat: attempt to get artists and display to home pages

--- handlers/index.go
+++ handlers/index.go
@@ -1,19 +1,55 @@
 package groupie
 
 import (
-	"net/http"
+	"encoding/json"
 	"html/template"
+	"net/http"
 )
 
+// Define a struct to match the structure of the API response
+type Artist struct {
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   int      `json:"firstAlbum"`
+	Members      []string `json:"members"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	// Fetch data from the API
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+		http.Error(w, "Failed to fetch data", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	// // Read and parse the JSON response
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	http.Error(w, "Failed to read response", http.StatusInternalServerError)
+	// 	return
+	// }
+	// fmt.Println(string(resp.Body))
+	var artists []Artist
+	err = json.NewDecoder(resp.Body).Decode(&artists)
+	if err != nil {
+		http.Error(w, "Failed to parse JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Load and parse the template
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// data := AsciiData{}
-	err = tmpl.Execute(w, nil)
+	// Execute the template with the data
+	err = tmpl.Execute(w, artists)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}

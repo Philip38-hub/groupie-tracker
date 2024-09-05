a6faf1a8165b6af5a294880266ddac77214e9ce7
Philip <oumaphilip01@gmil.com>
2024-09-05 10:39:34 +0300

    feat: visualize cards in index.html

--- handlers/index.go
+++ handlers/index.go
@@ -3,7 +3,9 @@ package groupie
 import (
 	"encoding/json"
 	"html/template"
+	"io"
 	"net/http"
+	"time"
 )
 
 // Define a struct to match the structure of the API response
@@ -11,7 +13,7 @@ type Artist struct {
 	Image        string   `json:"image"`
 	Name         string   `json:"name"`
 	CreationDate int      `json:"creationDate"`
-	FirstAlbum   int      `json:"firstAlbum"`
+	FirstAlbum   string   `json:"firstAlbum"`
 	Members      []string `json:"members"`
 	Locations    string   `json:"locations"`
 	ConcertDates string   `json:"concertDates"`
@@ -19,27 +21,40 @@ type Artist struct {
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	// Fetch data from the API
-	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	// Create a custom HTTP client with a timeout
+	client := &http.Client{
+		Timeout: 10 * time.Second, // 10-second timeout
+	}
+
+	// Make the GET request with the custom client
+	resp, err := client.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
-		http.Error(w, "Failed to fetch data", http.StatusInternalServerError)
+		http.Error(w, "Failed to fetch data: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
 
-	// // Read and parse the JSON response
-	// body, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	http.Error(w, "Failed to read response", http.StatusInternalServerError)
-	// 	return
-	// }
-	// fmt.Println(string(resp.Body))
+	// Read and parse the JSON response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Failed to read response", http.StatusInternalServerError)
+		return
+	}
+	// fmt.Println(string(body))
 	var artists []Artist
-	err = json.NewDecoder(resp.Body).Decode(&artists)
+	err = json.Unmarshal(body, &artists)
 	if err != nil {
 		http.Error(w, "Failed to parse JSON", http.StatusInternalServerError)
 		return
 	}
+
+	// Decode the JSON response directly
+	// var artists []Artist
+	// err = json.NewDecoder(resp.Body).Decode(&artists)
+	// if err != nil {
+	// 	http.Error(w, "Failed to parse JSON: "+err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
 
 	// Load and parse the template
 	tmpl, err := template.ParseFiles("templates/index.html")
